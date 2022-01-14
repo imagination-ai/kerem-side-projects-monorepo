3ec39d14cfb6fd5bc76e951befe7fb753909fb73
Kerem Baskaya <kerem@Kerems-MacBook-Pro.local>
2022-01-14 09:58:36 +0300

    Address the all issued dataset downloader

--- inflation/dataset_download.py
+++ inflation/dataset_download.py
@@ -1,11 +1,10 @@
+import datetime
 import logging
 from inflation.dataset.common_crawl import CommonCrawlerDownloader
 from common.customized_logging import configure_logging
 
 configure_logging()
 logger = logging.getLogger(__name__)
-
-url = "a101.com.tr/*"
 
 downloader = CommonCrawlerDownloader()
 
@@ -14,36 +13,31 @@ def run():
     import argparse
 
     parser = argparse.ArgumentParser(description="download crawler data")
-    parser.add_argument(
-        "--url",
-        default="json",
-        nargs="?",
-        choices=("json", "warc"),
-        type=str,
-        help="url of the web page",
-    )
+    parser.add_argument("--url", type=str, help="url of the web page")
     parser.add_argument("--output-fn", type=str, help="target file")
-    # choices Makefile #type , limit
+    parser.add_argument(
+        "--type",
+        type=str,
+        choices=("json", "warc"),
+        help="file type of the target file",
+    )
+    parser.add_argument("--limit", type=int, help="limit the download")
 
     args = parser.parse_args()
     logger.info(f"{args}")
 
-    if not any([args.url, args.output_fn]):
-        logger.error("Either --url and --output-fn should be provided.")
-
-    # if
-
-    downloader.download_partially_as_json(
-        url=args.url, output_fn=args.output_fn
-    )
-    # else:
-    #     downloader.download_as_warc(
-    #         url=url,
-    #         warc_description="a101.com.tr/* dataset (run at Jan, 10 2022)",
-    #         writer_prefix="a101-all",
-    #         writer_subprefix="suffix",
-    #         # limit=50,
-    #     )
+    if args.type == "json":
+        downloader.download_partially_as_json(
+            url=args.url, output_fn=args.output_fn, limit=args.limit
+        )
+    else:
+        downloader.download_as_warc(
+            url=args.url,
+            warc_description=f"{args.url} dataset (run at {datetime.datetime.now()})",
+            writer_prefix="data",
+            writer_subprefix="",
+            limit=args.limit,
+        )
 
 
 if __name__ == "__main__":
