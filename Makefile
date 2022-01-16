5c77ab80097b3158404471eb9b44e329595eed64
Kerem Baskaya <kerembaskaya@gmail.com>
2022-01-16 17:32:47 +0300

    Add reader script and update Makefile

--- Makefile
+++ Makefile
@@ -17,8 +17,14 @@ install-dev: install-test
 install-test: install
 	$(RUN) pip install -r requirements-test.txt
 
-download-dataset:
-	python -m inflation.dataset_download --url "a101.com.tr/*" --output-fn a101.json.gz --type json --limit 1 # Update limit in the long run
+inflation/dataset/a101.json.gz:
+	python -m inflation.dataset_download --url "a101.com.tr/*" --output-fn $@ --type json --limit 50000
+
+#inflation/dataset/%.json.gz:
+#	python -m inflation.dataset_download --url ${URL} --output-fn $@ --type json --limit 1 # Update limit in the long run
 
 clean:
 	rm -rf $(ENV)
+
+kerem-%: .gitignore
+	echo $@ $* $<
--- inflation/dataset/reader.py
+++ inflation/dataset/reader.py
@@ -1,5 +1,9 @@
+import gzip
+import json
 from dataclasses import dataclass
 from datetime import datetime
+
+from bs4 import BeautifulSoup
 
 
 class BaseJSONDataReader:
@@ -7,8 +11,102 @@ class BaseJSONDataReader:
         raise NotImplementedError()
 
 
+@dataclass
+class InflationDataRecord:
+    product_name: str
+    product_url: str
+    product_code: str  # item identifier for this data.
+    product_brand: str
+    price: float
+    currency: str
+    stock: str
+    sample_date: str  # Q: datetime.datetime niyeyse olmuyor? # price was valid for this date.
+
+
+class InflationDataset:
+    def __init__(self, data: list):
+        self.dataset = data
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __iter__(self):
+        return InflationDatasetIterator(self.dataset)
+
+    def __getitem__(self, s):
+        if s < 0:
+            s = self.__len__() - s
+            if s < 0 or s >= self.__len__():
+                raise IndexError
+            else:
+                return self.dataset[s]
+        else:
+            start, stop, step = s.indices(self.__len__())
+            indxs = list(range(start, stop, step))
+            return [self.dataset[i] for i in indxs]
+
+
+class InflationDatasetIterator:
+    def __init__(self, inflation_dataset):
+        self.dataset = inflation_dataset
+        self._i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._i >= len(self.dataset):
+            raise StopIteration
+        else:
+            result = self.dataset[self._i]
+            self._i += 1
+            return result
+
+
 class InflationJSONDatasetReader(BaseJSONDataReader):
-    def read(self, filename, fields=None):
+    @staticmethod
+    def __get_product_name(soup):
+        return soup.find("meta", property="og:title").attrs["content"]
+
+    @staticmethod
+    def __get_product_url(soup):
+        return soup.find("meta", property="og:url").attrs["content"]
+
+    @staticmethod
+    def __get_product_code(soup):
+        return (
+            soup.find("div", {"class": "product-code"})
+            .get_text()
+            .split(": ")[1]
+        )
+
+    @staticmethod
+    def __get_product_brand(soup):
+        return soup.find("meta", property="og:brand").attrs["content"]
+
+    @staticmethod
+    def __get_product_price(soup):
+        return float(
+            soup.find("meta", property="product:price:amount").attrs["content"]
+        )
+
+    @staticmethod
+    def __get_currency(soup):
+        return soup.find("meta", property="product:price:currency").attrs[
+            "content"
+        ]
+
+    @staticmethod
+    def __item_in_stock(soup):
+        return soup.find("meta", property="product:availability").attrs[
+            "content"
+        ]
+
+    @staticmethod
+    def __convert_sample_date(date: str):
+        return datetime.strptime(date, "%Y%m%d%H%M%S")
+
+    def read(self, filename, fields=None) -> InflationDataset:
         """It goes through the `filename` and create InflationRecords
 
         Args:
@@ -18,29 +116,23 @@ class InflationJSONDatasetReader(BaseJSONDataReader):
         Returns:
 
         """
-        # TODO (kerem)
-        pass
-        # return InflationData(data)
+        records = []
+        with gzip.open(filename, "rt", encoding="UTF-8") as zipfile:
+            for line in zipfile:
+                line = json.loads(line)
+                date = line["timestamp"]  # alttakini acinca silinecek
+                # date = InflationJSONDatasetReader.__convert_sample_date(line['timestamp']) # Bunu dataclass'taki str type'ini datetime.datetime ile degistirince acacagim
+                soup = BeautifulSoup(line["text"])
+                record = InflationDataRecord(
+                    InflationJSONDatasetReader.__get_product_name(soup),
+                    InflationJSONDatasetReader.__get_product_url(soup),
+                    InflationJSONDatasetReader.__get_product_code(soup),
+                    InflationJSONDatasetReader.__get_product_brand(soup),
+                    InflationJSONDatasetReader.__get_product_price(soup),
+                    InflationJSONDatasetReader.__get_currency(soup),
+                    InflationJSONDatasetReader.__item_in_stock(soup),
+                    date,
+                )
+                records.append(record)
 
-
-class InflationDataRecord(dataclass):
-    price: float
-    currency: str
-    item: str  # item identifier for this data.
-    sample_date: datetime  # price was valid for this date.
-    # ...
-
-
-class InflationDataset:
-    def __init__(self, data):
-        self.dataset = data
-
-    def __iter__(self):
-        # TODO (kerem)
-        pass
-
-    def __get__(self, idx):
-        return self.dataset[idx]
-
-    def __len__(self):
-        return len(self.dataset)
+        return InflationDataset(records)
