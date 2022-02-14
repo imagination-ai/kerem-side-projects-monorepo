0053c8cb4fdf31ee0c73e91d8e1bd271f1c31664
Kerem Baskaya <kerembaskaya@gmail.com>
2022-02-14 10:09:14 +0300

    Add crawler's  test

--- inflation-resources/tests/dataset/test_crawl.py
+++ inflation-resources/tests/dataset/test_crawl.py
@@ -1,12 +1,29 @@
 import pytest
-from inflation.dataset.crawl import Crawler
-
+import tempfile
+import os
+from unittest import mock
 from pathlib import Path
-
+from inflation.dataset.crawl import Crawler
+from inflation.dataset.crawl import ItemRecord
 
 TEST_LINK_EXCEL = (
     Path(__file__).parents[3] / "inflation-resources/tests/data/test_links.xlsx"
 )
+
+TEST_PAGE = (
+    Path(__file__).parents[3]
+    / "inflation-resources/tests/data/pages/Ovadan-Pirinç-Baldo-1000-G-A101.html"
+)
+
+rec = [ItemRecord("123", "pirinc", "ovadan", "http://www.gmail.com", "A101")]
+
+
+@pytest.fixture(scope="function")
+def html_test_file():
+    import codecs
+
+    f = codecs.open(TEST_PAGE, "r").read()
+    return f
 
 
 @pytest.fixture(scope="module")
@@ -29,3 +46,24 @@ def test_crawl_parse_true_items(records):
         items_codes.append(record.item_code)
 
     assert len(set(items_codes)) == 5
+
+
+@mock.patch("inflation.dataset.crawl.requests.get")
+def test_crawl_output(mock_requests_get, crawler, html_test_file):
+    """
+    (1) It tests the Crawler's crawl method creates output file (a json).
+    (2) It tests this file exist or not.
+    """
+
+    mock_requests_get.return_value = mock.Mock(
+        status_code=200, text=html_test_file
+    )
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        fn = tmpdirname + "/" + "test"
+        crawler.crawl(rec, fn)
+        files = []
+        for entry in os.listdir(tmpdirname):
+            if os.path.isfile(os.path.join(tmpdirname, entry)):
+                files.append(entry)
+        print(files)
+        assert len(files) == 1
