9bd5a3298ddf1ac19abd2e6c9694fff60da188dc
Kerem Baskaya <kerembaskaya@gmail.com>
2022-02-17 18:56:05 +0300

    Starting part for migros-crawler

--- inflation/dataset/reader.py
+++ inflation/dataset/reader.py
@@ -8,8 +8,15 @@ import pandas as pd
 from dataclasses import dataclass
 from pathlib import Path
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from common.customized_logging import configure_logging
+from common.utils.mathematical_conversation_utils import (
+    convert_turkish_number_to_us,
+)
 
 SAVED_DATASETS_DIR_PATH = (
     Path(__file__).parents[2] / "inflation-resources/data/"
@@ -228,10 +235,115 @@ class InflationJSONA101DatasetReader(BaseJSONDataReader):
         return InflationDataset(records)
 
 
+class InflationJSONMigrosDatasetReader(BaseJSONDataReader):
+    ITEM_IN_STOCK = "InStock"
+    ITEM_NOT_IN_STOCK = (
+        "OutStock"  # TODO: Need to check. It's only a guess right now.
+    )
+
+    def read_migros_page(self):  # url, driver_path, driver (quit demek icin)
+        driver = webdriver.Chrome(
+            executable_path="/Users/kerem/playground/kerem-side-projects-monorepo/common/chromedriver"
+        )
+        url = "https://www.migros.com.tr/pinar-organik-sut-1-l-p-a822f9"
+        driver.get(url)
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "price"))
+            )
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            soup = soup.find("script", type="application/json+ld")
+            page_json = json.loads(
+                soup.string
+            )  # json contains all related information
+            subset_page_json = page_json["mainEntity"]["offers"]["itemOffered"][
+                0
+            ]
+
+            print(
+                InflationJSONMigrosDatasetReader.__get_product_name(
+                    subset_page_json
+                )
+            )
+            print(
+                InflationJSONMigrosDatasetReader.__get_product_url(
+                    subset_page_json
+                )
+            )
+            print(
+                InflationJSONMigrosDatasetReader.__get_product_code(
+                    subset_page_json
+                )
+            )
+            print(
+                InflationJSONMigrosDatasetReader.__get_product_brand(
+                    subset_page_json
+                )
+            )
+            print(
+                InflationJSONMigrosDatasetReader.__get_product_price(
+                    subset_page_json
+                )
+            )
+            print(
+                InflationJSONMigrosDatasetReader.__get_currency(
+                    subset_page_json
+                )
+            )
+            print(
+                InflationJSONMigrosDatasetReader.__item_in_stock(
+                    subset_page_json
+                )
+            )
+
+        finally:
+            driver.quit()
+
+        return subset_page_json
+
+    @staticmethod
+    def __get_product_name(subset_page_json):
+        return subset_page_json["name"]
+
+    @staticmethod
+    def __get_product_url(subset_page_json):
+        return subset_page_json["url"]
+
+    @staticmethod
+    def __get_product_code(subset_page_json):
+        return subset_page_json["image"][0]["contentUrl"].split("/")[-2]
+
+    @staticmethod
+    def __get_product_brand(subset_page_json):
+        return subset_page_json["brand"]["name"]
+
+    @staticmethod
+    def __get_product_price(subset_page_json):
+        return convert_turkish_number_to_us(subset_page_json["offers"]["price"])
+
+    @staticmethod
+    def __get_currency(subset_page_json):
+        return subset_page_json["offers"]["priceCurrency"]
+
+    @staticmethod
+    def __item_in_stock(subset_page_json):
+        availability = subset_page_json["offers"]["availability"].split("/")[-1]
+        if availability == InflationJSONMigrosDatasetReader.ITEM_IN_STOCK:
+            return True
+        elif availability == InflationJSONMigrosDatasetReader.ITEM_NOT_IN_STOCK:
+            pass
+            return False
+        else:
+            print(subset_page_json["offers"]["availability"])
+            raise TypeError
+
+
 def run():
-    reader = InflationJSONA101DatasetReader()
-    dataset = reader.read("a101.med.json.gz")
-    print(dataset)
+    # reader = InflationJSONA101DatasetReader()
+    # dataset = reader.read("a101.med.json.gz")
+    # print(dataset)
+    migros_reader = InflationJSONMigrosDatasetReader()
+    migros_reader.read_migros_page()
 
 
 if __name__ == "__main__":
