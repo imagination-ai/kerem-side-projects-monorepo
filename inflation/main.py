4cd87ab7aba99a6c90e3663d11b4ab57b9a8c640
Kerem Baskaya <kerembaskaya@gmail.com>
2022-03-04 22:39:14 +0300

    Add parser endpoint and its function

--- inflation/main.py
+++ inflation/main.py
@@ -1,30 +1,40 @@
 from datetime import datetime
 import logging
 import os
+import gzip
+import json
 
 from fastapi import BackgroundTasks, FastAPI
 
 from common.clients.google_storage_client import GoogleStorageClient
 from common.customized_logging import configure_logging
 from inflation.config import settings
-from inflation.dataset.crawl import A101Crawler, CrawlerManager, MigrosCrawler
+from inflation.dataset.crawl import CrawlerManager, A101Crawler, MigrosCrawler
+from inflation.dataset.parse import ParserManager, A101Parser, MigrosParser
 
 
-BUCKET_NAME = os.getenv("CRAWLER_BUCKET", "inflation-project-crawler-output")
+CRAWLER_BUCKET = os.getenv("CRAWLER_BUCKET", "inflation-project-crawler-output")
+PARSER_BUCKET = os.getenv("PARSER_BUCKET", "inflation-project-parser-output")
 
 CRAWLERS = {"a101": A101Crawler(), "migros": MigrosCrawler()}
+PARSERS = {"a101": A101Parser(), "migros": MigrosParser()}
 
 configure_logging()
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 cm = CrawlerManager(CRAWLERS)
-storage_client = GoogleStorageClient(bucket_name=BUCKET_NAME)
+pm = ParserManager(PARSERS)
+crawler_client = GoogleStorageClient(bucket_name=CRAWLER_BUCKET)
+parser_client = GoogleStorageClient(bucket_name=PARSER_BUCKET)
 
 OUTPUT_PATH = "/applications/downloaded-files/"
 
 logger.info(
-    f"Starting the application with -- Crawlers:{CRAWLERS}, Bucket Name:{BUCKET_NAME}"
+    f"Starting the application with -- Crawlers:{CRAWLERS}, Bucket Name:{CRAWLER_BUCKET}"
+)
+logger.info(
+    f"Starting the application with -- Parsers:{PARSERS}, Bucket Name:{PARSER_BUCKET}"
 )
 
 
@@ -47,10 +57,41 @@ def fetch_inflation_data(excel_path, output_path, filename):
     inflation_fn = cm.start_crawling(records, output_path, filename)
     basename = os.path.basename(inflation_fn)
 
-    logger.info(f"Uploading {inflation_fn} to {BUCKET_NAME}/{basename}")
+    logger.info(f"Uploading {inflation_fn} to {CRAWLER_BUCKET}/{basename}")
 
-    storage_client.upload(inflation_fn, basename)
-    logger.info(f"Crawling done for {excel_path}: {BUCKET_NAME}/{basename}")
+    crawler_client.upload(inflation_fn, basename)
+    logger.info(f"Crawling done for {excel_path}: {CRAWLER_BUCKET}/{basename}")
+
+
+def parse_inflation_data(
+    source_filename, destination_filename="inflation-resources/data/inflation/"
+):
+    parser_client.download(
+        source_filename, destination_filename=destination_filename
+    )
+    logger.info(
+        f"Downlading {source_filename} to {CRAWLER_BUCKET}/{destination_filename}"
+    )
+
+    crawled_inflation_data_fn = os.path.join(
+        destination_filename, source_filename
+    )
+    parsed_inflation_data = pm.start_parsing_from_drive(
+        crawled_inflation_data_fn
+    )  # Q: start parsing data donuyor orada json kaydedip yine adres donmek daha mantikli olabilir. O zaman alttaki kisimlar tasinacak.
+
+    parsed_inflation_data_fn = os.path.join(
+        destination_filename, f"{source_filename}.json.gz"
+    )
+    logger.info(f"Parsing done for {source_filename}")
+
+    with gzip.open(parsed_inflation_data_fn, "wt") as file:
+        data = json.dumps(parsed_inflation_data)
+        file.write(data)
+
+    basename = os.path.basename(parsed_inflation_data_fn)
+    parser_client.upload(parsed_inflation_data_fn, basename)
+    logger.info(f"Uploading {source_filename} to {PARSER_BUCKET}/{basename}")
 
 
 def collect_db_stats(db_path):
@@ -89,7 +130,7 @@ async def fetch_data_async(
     )
     return {
         "success": True,
-        "message": f"{BUCKET_NAME}/{filename} is preparing.",
+        "message": f"{CRAWLER_BUCKET}/{filename} is preparing.",
     }
 
 
@@ -102,7 +143,19 @@ async def fetch_data(
     fetch_inflation_data(excel_path, OUTPUT_PATH, filename)
     return {
         "success": True,
-        "message": f"{BUCKET_NAME}/{filename} is preparing.",
+        "message": f"{CRAWLER_BUCKET}/{filename} is preparing.",
+    }
+
+
+@app.get("/Parse", tags=["Parse"])
+async def parse_data(
+    source_filename, destination_filename="inflation-resources/data/inflation/"
+):
+
+    parse_inflation_data(source_filename, destination_filename)
+    return {
+        "success": True,
+        "message": f"{PARSER_BUCKET}/{source_filename} is preparing.",
     }
 
 
