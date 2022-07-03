c651b51152705361b94f592b99031df5f923ebfd
Osman Baskaya <osmanbaskaya1@gmail.com>
2022-07-03 10:29:17 +0300

    fixing entrypoint for style trainer

--- entrypoints/style-trainer-entrypoint.sh
+++ entrypoints/style-trainer-entrypoint.sh
@@ -7,4 +7,5 @@ gsutil cp gs://projects-misc/datasets/style-datasets/book_ds.tar.gz .
 tar -xzvf book_ds.tar.gz
 mv book_ds style-resources/datasets/book_ds
 rm -rf book_ds.tar.gz
-jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --port 8080
+#jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --port 8080
+python -m style.train.classifier_trainer
