51fb8e7bfe71d226d79e51e6d212228982d0f2ce
Kerem Baskaya <kerembaskaya@gmail.com>
2022-07-04 18:25:22 +0300

    Make parameterize number of workers (#35)

--- style/config.py
+++ style/config.py
@@ -1,3 +1,5 @@
+import os
+
 from common.config import CommonSettings
 
 
@@ -10,6 +12,7 @@ class StyleAppSettings(CommonSettings):
     NUM_DOC: int = 100
     NGRAM_MIN: int = 1
     NGRAM_MAX: int = 2
+    N_JOBS: int = os.cpu_count() - 1
 
 
 style_app_settings = StyleAppSettings(_env_file=".env")
